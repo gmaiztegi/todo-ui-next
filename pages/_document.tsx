88a1ead286b3941d77ace064372e8f585beb187e
Gorka Maiztegi Etxeberria <gmaiztegi@gmail.com>
2019-02-08 12:52:32 +0100

    Add support for typed components for next.js

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,21 +1,24 @@
-import Document, { Head, Main, NextScript } from "next/document";
+import Document, { NextDocumentContext } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
-  render() {
-    return (
-      <html>
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <title>Magnificent Todo-List</title>
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </html>
-    );
+  static async getInitialProps(ctx: NextDocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [...initialProps.styles!, ...sheet.getStyleElement()]
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 }
