73bdf0edf0f4be48b7e749d53db98c476e14933a
Gorka Maiztegi Etxeberria <gmaiztegi@gmail.com>
2019-02-13 15:45:12 +0100

    Add testing infraestructure

---
+++ __tests__/pages/index.spec.tsx
@@ -0,0 +1,19 @@
+import { mount } from "enzyme";
+import getConfig, { RuntimeConfig } from "next/config";
+import IndexPage from "../../pages/index";
+jest.mock("next/config");
+
+describe("Index", () => {
+  beforeAll(() => {
+    getConfig.mockImplementation(
+      (): RuntimeConfig => {
+        return { publicRuntimeConfig: {}, serverRuntimeConfig: {} };
+      }
+    );
+  });
+
+  it("should render without throwing an error", () => {
+    const wrap = mount(<IndexPage todos={[]} />);
+    expect(wrap.find("h1").text()).toBe("To-do list");
+  });
+});
