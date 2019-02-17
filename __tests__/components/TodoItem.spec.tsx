5ebab3d48b8ce75d4048dadf88afcfdc82045766
Gorka Maiztegi Etxeberria <gmaiztegi@gmail.com>
2019-02-18 00:39:21 +0100

    Add TotoItem test

---
+++ __tests__/components/TodoItem.spec.tsx
@@ -0,0 +1,24 @@
+import { mount } from "enzyme";
+import { TodoItem } from "../../components";
+
+describe("TodoItem", () => {
+  it("should render the label", () => {
+    const label = "label1";
+
+    const wrap = mount(<TodoItem label={label} onDelete={() => {}} />);
+
+    expect(wrap.find("p").text()).toEqual(label);
+  });
+
+  it("should call the delete callback when cross is clicked", () => {
+    const callback = jest.fn();
+    const props = { label: "label", onDelete: callback };
+
+    const wrap = mount(<TodoItem {...props} />);
+    const deleteButton = wrap.find("button");
+    expect(deleteButton.text()).toEqual("×");
+
+    deleteButton.simulate("click");
+    expect(callback).toBeCalledTimes(1);
+  });
+});
