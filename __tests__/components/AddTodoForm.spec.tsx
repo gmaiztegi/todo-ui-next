9901bf9de202d43f3d2d78fa97dac4accf8a98dd
Gorka Maiztegi Etxeberria <gmaiztegi@gmail.com>
2019-02-18 10:28:07 +0100

    Add AddTodoForm test

---
+++ __tests__/components/AddTodoForm.spec.tsx
@@ -0,0 +1,21 @@
+import { mount } from "enzyme";
+import AddTodoForm from "../../components/AddTodoForm";
+
+describe("AddTodoForm", () => {
+  it("should call the callback and clear the text when submited", () => {
+    const typedText = "test1";
+    const callback: jest.Mock<void, [string]> = jest.fn();
+
+    const wrap = mount(<AddTodoForm onCreate={callback} />);
+    const input = wrap.find("input");
+    input.simulate("change", { target: { value: typedText } });
+    const stopPropagation: jest.Mock<void, []> = jest.fn();
+    const preventDefault: jest.Mock<void, []> = jest.fn();
+    wrap.find("form").simulate("submit", { stopPropagation, preventDefault });
+
+    expect(callback).toBeCalledWith(typedText);
+    expect(input.prop<string>("value")).toEqual("");
+    expect(stopPropagation).toHaveBeenCalled();
+    expect(preventDefault).toHaveBeenCalled();
+  });
+});
